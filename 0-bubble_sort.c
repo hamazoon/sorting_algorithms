55e3557362038e3514f8405a9e877d495b89fedc
IbtissamErrachidi <ibtissamerrachidi810@gmail.com>
2023-12-14 07:22:04 +0000

    updat

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -17,7 +17,7 @@ void bubble_sort(int *array, size_t size)
 
 	for (i = 0; i < size - 1; i++)
 	{
-		flag = 0 ;
+		flag = 0;
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
@@ -26,10 +26,10 @@ void bubble_sort(int *array, size_t size)
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
 				print_array(array, size);
-				flag = 1 ;
+				flag = 1;
 			}
 		}
-		if (flag == 0 ) 
+		if (flag == 0)
 			break;
 	}
 }
