5f25bef1dc2385b6725018556f01740fd90c3852
hamazoon <hamazoon1.gmil.com>
2023-12-19 14:35:27 -0800

    header update

--- sort.h
+++ sort.h
@@ -14,9 +14,9 @@
  */
 typedef struct listint_s
 {
-		const int n;
-			struct listint_s *prev;
-				struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 /*printing tools*/
 void print_list(const listint_t *list);
@@ -31,6 +31,7 @@ void quick_sort(int *array, size_t size);
 void quicksort(int *array, int low, int heigh, size_t size);
 int partition(int *array, int low, int high, size_t size);
 void swap(int *array, size_t a, size_t b);
+void shell_sort(int *array, size_t size);
 
 /*Cocktail shaker sort functions*/
 void cocktail_sort_list(listint_t **list);
